91137a37445f00be1c6eb9b9598553c40b49dcab
tokuzou0829 <77785208+tokuzou0829@users.noreply.github.com>
2026-04-18 04:06:48 +0900

    DLsiteタイトルの変換機能を追加し、メッセージ処理を更新

--- packages/bot/index.ts
+++ packages/bot/index.ts
@@ -3,6 +3,7 @@ import { commands } from './commands/commands.js';
 import { connections } from './commands/join.js';
 import { createAudioResource } from '@discordjs/voice';
 import { generateVoice } from './lib/generate.js';
+import { conversionMessage } from './lib/conversionMessage.js';
 
 
 const client = new Client({ intents: [
@@ -27,9 +28,10 @@ client.on(Events.InteractionCreate, async (interaction: Interaction) => {
 
 client.on(Events.MessageCreate, async (message: Message) => {
   if (message.author.bot) return;
+  const messageText = await conversionMessage(message.content);
   if (connections.some(vc => vc.targetChannel !== message.channel.id)) return;
   const { player } = connections.find(vc => vc.targetChannel === message.channel.id)!;
-  const voice = await generateVoice(message.content, '874568803256786945');
+  const voice = await generateVoice(messageText, '874568803256786945');
   if (!voice) return;
   const audioResouce = createAudioResource(voice);
   player.play(audioResouce)
---
+++ packages/bot/lib/getDlsiteTitle.ts
@@ -0,0 +1,9 @@
+export async function getDlsiteTitle(rj_code:string){
+    try {
+        const request = await fetch(`https://www.dlsite.com/maniax/api/=/product.json?workno=${rj_code}`);
+        const data = await request.json();
+        return data.work_name_kana || data.work_name;
+    } catch {
+        return rj_code;
+    }
+}
