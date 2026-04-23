f7c6132f66526b1895c529729102167658ff8cb4
Soma Noda <me@odango.app>
2026-04-23 12:28:08 +0900

    tree-fmtを実装 (#23)
    * feat: setup tree-fmt
    
    * chore: actionsでtreefmtを使用するように変更

--- packages/server/main.py
+++ packages/server/main.py
@@ -6,23 +6,25 @@ from pydantic import BaseModel
 from omnivoice import OmniVoice
 import torch
 
+
 class GenerateParams(BaseModel):
     text: str
     speaker: str
 
+
 model = OmniVoice.from_pretrained(
-    "k2-fsa/OmniVoice",
-    device_map="cuda:0",
-    dtype=torch.float16
+    "k2-fsa/OmniVoice", device_map="cuda:0", dtype=torch.float16
 )
 
 transcript = speaker.get_transcript()
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
 
 @app.post("/generate")
 def generateVoice(params: GenerateParams):
@@ -32,15 +34,15 @@ def generateVoice(params: GenerateParams):
         text=params.text,
         ref_audio=f"{speaker.BASE_PATH}/{params.speaker}.wav",
         ref_text=transcript[params.speaker] if params.speaker in transcript else None,
-        language_id=262
+        language_id=262,
     )
 
     buf = io.BytesIO()
     sf.write(buf, audio[0], 24000, format="WAV")
     return Response(content=buf.getvalue(), media_type="audio/wav")
 
+
 @app.get("/speaker_list", response_model=list[str])
 def get_speaker_list():
-    ids:list[str] = [x.id for x in transcript]
+    ids: list[str] = [x.id for x in transcript]
     return ids
-
