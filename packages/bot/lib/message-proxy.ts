61985b2d48ead2399dd1b474f5e8254ecd8c1fe3
Soma Noda <me@odango.app>
2026-04-25 00:53:29 +0900

    Refactor/message proxy (#28)
    * refactor: message pipeline
    
    * refactor: ファイル名をいい感じにした
    
    * fix: importするファイルを変更するの忘れてた
    
    * fix: pipelineになってなかった

---
+++ packages/bot/lib/message-proxy.ts
@@ -0,0 +1,30 @@
+import { getDlsiteTitle } from "./get-dlsite-title.js";
+
+type MaybePromise<T> = T | Promise<T>;
+type MessageProxy = (text: string) => MaybePromise<string>;
+
+const replaceRJCodes: MessageProxy = async (text: string) => {
+  const regex = /rj\d+/gi;
+  const matches = [...text.matchAll(regex)];
+
+  let result = text;
+
+  for (const match of matches) {
+    const rjCode = match[0].toUpperCase();
+    const title = await getDlsiteTitle(rjCode);
+    result = result.replace(match[0], title);
+  }
+
+  return result;
+};
+
+const replaceTilde: MessageProxy = (text: string) => text.replaceAll(/[~〜]/g, "ー");
+
+export const conversionMessage: MessageProxy = async (text: string) => {
+  const pipeline: MessageProxy[] = [replaceTilde, replaceRJCodes];
+  let replaced = text;
+  for (const replace of pipeline) {
+    replaced = await replace(replaced);
+  }
+  return replaced;
+};
